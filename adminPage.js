5a7d0b39d1cd5a71a3288c06977889fca9502ff5
vinay <vinay.v@darwinbox.io>
2023-01-25 00:40:54 +0530

    "adding all required functionality"

--- adminPage.js
+++ adminPage.js
@@ -1,4 +1,4 @@
-//post function
+//GET function
 
 const httRequest = new XMLHttpRequest();
 let tablebody = document.getElementById("tablebody");
@@ -7,7 +7,7 @@ httRequest.send();
 var data = "empty"
 httRequest.onload = function() {
     data = JSON.parse(httRequest.responseText);
-    console.log(data);
+    console.log(data);[  ]
     users_data = data.data;
     for(var i=0; i<users_data.length; i++){
         tablebody.innerHTML += renderUser(users_data[i].email,users_data[i].first_name,users_data[i].last_name);
@@ -24,3 +24,23 @@ function renderUser(email, first_name,last_name)
     </tr>
     `
 }
+
+userListbtn = document.getElementById("userListbtn");
+addUserbtn = document.getElementById("addUserbtn");
+addServicebtn = document.getElementById("addServicebtn");
+
+userListbtn.addEventListener("click", function(){
+    document.getElementById("addUser").style.display="none";
+    document.getElementById("addService").style.display="none";
+    document.getElementById("userList").style.display="inline";  
+})
+addUserbtn.addEventListener("click",function(){
+    document.getElementById("userList").style.display="none";
+    document.getElementById("addService").style.display="none";
+    document.getElementById("addUser").style.display="inline";
+})
+addServicebtn.addEventListener("click",function(){ 
+    document.getElementById("userList").style.display="none";
+    document.getElementById("addUser").style.display="none";
+    document.getElementById("addService").style.display="inline";
+})
