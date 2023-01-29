ef59c0f34e7557e448e7d9d1e86a73fc3599fb3b
vinay <vinay.v@darwinbox.io>
2023-01-29 21:35:42 +0530

    ""

--- adminPage.js
+++ adminPage.js
@@ -2,20 +2,19 @@
 
 const httRequest = new XMLHttpRequest();
 let tablebody = document.getElementById("tablebody");
-httRequest.open("GET","http:127.0.0.1:3000/userdata");
+httRequest.open("GET", "http:127.0.0.1:3000/userdata");
 httRequest.send();
 var data = "empty"
-httRequest.onload = function() {
+httRequest.onload = function () {
     data = JSON.parse(httRequest.responseText);
     console.log(data);
     users_data = data.data;
-    for(var i=0; i<users_data.length; i++){
-        tablebody.innerHTML += renderUser(users_data[i].email,users_data[i].first_name,users_data[i].last_name);
+    for (var i = 0; i < users_data.length; i++) {
+        tablebody.innerHTML += renderUser(users_data[i].email, users_data[i].first_name, users_data[i].last_name);
     }
 }
 
-function renderUser(email, first_name,last_name)
-{
+function renderUser(email, first_name, last_name) {
     return `
     <tr>
     <td>${first_name}</td>
@@ -29,18 +28,18 @@ userListbtn = document.getElementById("userListbtn");
 addUserbtn = document.getElementById("addUserbtn");
 addServicebtn = document.getElementById("addServicebtn");
 
-userListbtn.addEventListener("click", function(){
-    document.getElementById("addUser").style.display="none";
-    document.getElementById("addService").style.display="none";
-    document.getElementById("userList").style.display="inline";  
+userListbtn.addEventListener("click", function () {
+    document.getElementById("addUser").style.display = "none";
+    document.getElementById("addService").style.display = "none";
+    document.getElementById("userList").style.display = "inline";
 })
-addUserbtn.addEventListener("click",function(){
-    document.getElementById("userList").style.display="none";
-    document.getElementById("addService").style.display="none";
-    document.getElementById("addUser").style.display="inline";
+addUserbtn.addEventListener("click", function () {
+    document.getElementById("userList").style.display = "none";
+    document.getElementById("addService").style.display = "none";
+    document.getElementById("addUser").style.display = "inline";
 })
-addServicebtn.addEventListener("click",function(){ 
-    document.getElementById("userList").style.display="none";
-    document.getElementById("addUser").style.display="none";
-    document.getElementById("addService").style.display="inline";
+addServicebtn.addEventListener("click", function () {
+    document.getElementById("userList").style.display = "none";
+    document.getElementById("addUser").style.display = "none";
+    document.getElementById("addService").style.display = "inline";
 })
